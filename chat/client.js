e452fa0f1baec1b518058847cb0bd05500292803
Andrey <andreonof@yandex.ru>
2024-11-29 13:58:32 +0300

    first commit

---
+++ chat/client.js
@@ -0,0 +1,19 @@
+!function(){
+    var ws = new WebSocket('wss://websocket-echo.com/');
+
+    ws.onopen = function(){
+        console.log("Соединение установлено")
+    };
+
+    ws.onmessage = function(event){
+        console.log("Получены данные " + event.data);
+    };
+
+    ws.onmessage = function(event){
+        console.log(event.data);
+    }
+
+    ws.onerror = function(event){
+        console.log(event.data);
+    }
+}()
